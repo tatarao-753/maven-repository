0f7be0875c01a039dc9a7f383aecc9d9eb72d980
tata <tata123@gmail.com>
2023-10-26 14:04:41 +0000

    commited all files

---
+++ src/main/java/application_devlopment/App.java
@@ -0,0 +1,13 @@
+package application_devlopment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
